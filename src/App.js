1213b3e868d0d4af3572c12547971678698571d7
julie <julie@julies-MacBook-Pro.local>
2020-06-26 01:42:19 -0700

    remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Gallery from "./components/gallery/gallery"
-import Header from "./components/header/header"
-import NavigationBar from "./components/navigationbar"
 import Main from "./components/main"
 import { Route, Switch } from "react-router-dom"
 import "react-bulma-components/dist/react-bulma-components.min.css"
