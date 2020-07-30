cad16d25b10e4eea1f1e11f5feb35097f7cbc36d
julie <julie@julies-MacBook-Pro.local>
2020-07-29 22:13:05 -0700

    changed hours

--- src/components/hours.js
+++ src/components/hours.js
@@ -1,38 +1,38 @@
 const hours = [
     {
         day: "Sun",
-        open: "8:30am",
-        close: "9:00pm"
+        open: "9:00am",
+        close: "6:00pm"
     },
     {
         day: "Mon",
-        open: "8:30am",
-        close: "9:00pm"
+        open: "9:00am",
+        close: "6:00pm"
     },
     {
         day: "Tue",
-        open: "8:30am",
-        close: "9:00pm"
+        open: "9:00am",
+        close: "6:00pm"
     },
     {
         day: "Wed",
-        open: "8:30am",
-        close: "9:00pm"
+        open: "9:00am",
+        close: "6:00pm"
     },
     {
         day: "Thu",
-        open: "8:30am",
-        close: "10:00pm"
+        open: "9:00am",
+        close: "6:00pm"
     },
     {
         day: "Fri",
-        open: "8:30am",
-        close: "10:00pm"
+        open: "9:00am",
+        close: "7:00pm"
     },
     {
         day: "Sat",
-        open: "8:30am",
-        close: "11:00pm"
+        open: "9:00am",
+        close: "7:00pm"
     }
 ]
 
