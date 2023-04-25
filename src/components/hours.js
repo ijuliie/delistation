1df86e3a5cc0275ded7973728f16aa0e54630942
Julie Trinh <ijuliie06@gmail.com>
2023-04-25 12:33:01 -0700

    update site hours

--- src/components/hours.js
+++ src/components/hours.js
@@ -1,38 +1,38 @@
 const hours = [
     {
         day: "Sun",
-        open: "9:00am",
-        close: "6:00pm"
+        open: "10:00am",
+        close: "7:00pm"
     },
     {
         day: "Mon",
-        open: "9:00am",
-        close: "6:00pm"
+        open: "10:00am",
+        close: "7:00pm"
     },
     {
         day: "Tue",
-        open: "9:00am",
-        close: "6:00pm"
+        open: "10:00am",
+        close: "7:00pm"
     },
     {
         day: "Wed",
-        open: "9:00am",
-        close: "6:00pm"
+        open: "10:00am",
+        close: "7:00pm"
     },
     {
         day: "Thu",
-        open: "9:00am",
-        close: "6:00pm"
+        open: "10:00am",
+        close: "7:00pm"
     },
     {
         day: "Fri",
-        open: "9:00am",
-        close: "7:00pm"
+        open: "10:00am",
+        close: "8:00pm"
     },
     {
         day: "Sat",
-        open: "9:00am",
-        close: "7:00pm"
+        open: "10:00am",
+        close: "8:00pm"
     }
 ]
 
